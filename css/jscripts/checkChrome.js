7c77ee13729b3fe9db4c39e9871bfaf1640dd5cd
gkong1220 <gabrielmkong@gmail.com>
2020-08-01 16:24:25 -0700

    chrome check fix

--- css/jscripts/checkChrome.js
+++ css/jscripts/checkChrome.js
@@ -1,7 +1,16 @@
 $(function () {
-    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-    console.log(isChrome);
-    if (!isChrome) {
+
+    var winNav = window.navigator;
+    var vendorName = winNav.vendor;
+
+    var isChrome = window.chrome;
+    var isOpera = typeof window.opr !== "undefined";
+    var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+    var isIOSChrome = winNav.userAgent.match("CriOS");
+
+    var usingChrome = isIOSChrome || isChrome !== null && typeof isChrome !== "undefined" && vendorName == "Google Inc." && isOpera === false && isIEedge === false;
+    console.log(usingChrome);
+    if (!usingChrome) {
         window.alert("Hello :) This website is best experienced in Chrome. Some features may not be full supported.");
     }
 });
